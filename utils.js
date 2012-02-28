6a9f022785d7d65ad10216043fd8b29787eae9ad
Jamie Bliss <astronouth7303@gmail.com>
2012-02-28 18:03:21 +0000

    "Rewrote detection"

--- utils.js
+++ utils.js
@@ -1,36 +1,5 @@
-function addMethod(obj, func) {
-	if (!obj.__proto__[func.name]) {
-		obj.__proto__[func.name] = func;
-	}
+function isSubDomain(sub, dom) {
+	if (sub == dom) return true;
+	if (dom != sub.substr(-dom.length)) return false;
+	return sub[dom.length] == '.';
 }
-
-addMethod(String, function startsWith(str) {
-	return !this.indexOf(str);
-});
-
-addMethod(String, function contains(it) { 
-	return this.indexOf(it) != -1;
-});
-
-/*addMethod(Array, function uniquify() { 
-	this.sort();
-	for (var i = 1; i < this.length; i++) {
-		if (this[i-1] == this[i]) {
-			delete this[i--];
-		}
-	}
-	return this;
-});
-
-addMethod(Array, function contains(item) {
-	return this.indexOf(item) != -1;
-});
-
-addMethod(Array, function removeByValue(arr, val) {
-	for(var i=0; i<arr.length; i++) {
-		if(arr[i] == val) {
-			arr.splice(i, 1);
-			break;
-		}
-	}
-});*/
